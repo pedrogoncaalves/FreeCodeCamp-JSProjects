b483a7c7648655623ea2e98a0006c40a4bdfecc6
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-10-26 23:26:25 -0300

    ceaser cipher

---
+++ ceaserCipher.js
@@ -0,0 +1,22 @@
+function rot13(str) {
+  
+  const Acode = "A".charCodeAt();
+  const Ncode = "N".charCodeAt();
+  const Zcode = "Z".charCodeAt();
+  return [...str]
+    .map(function (e) {
+      const code = e.charCodeAt();
+      if (Acode <= code && code <= Zcode) {
+        if (code < Ncode) {
+          return String.fromCharCode(code + 13);
+        } else {
+          return String.fromCharCode(code - 13);
+        }
+      } else {
+        return e;
+      }
+    })
+    .join("");
+}
+
+rot13("SERR PBQR PNZC");
